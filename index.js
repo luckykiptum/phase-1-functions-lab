23c14e70dba0c5c9891c4b4cad3987a6f8350ff5
lucky <luckykiptum@gmail.com>
2024-03-21 17:00:50 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+  const hqLocation = 42;
+  return Math.abs(pickupLocation -hqLocation);
+}
+
+function distanceFromHqInFeet(pickupLocation){
+  const feetPerBlock = 264;
+  const blocks = distanceFromHqInBlocks(pickupLocation);
+  return blocks * feetPerBlock;
+}
+console.log(distanceFromHqInFeet(27)); 
+
+function distanceTravelledInFeet(start, destination) {
+  const feetPerBlock = 264;
+  const distanceTravelled = Math.abs( start-destination)
+  return distanceTravelled * feetPerBlock;
+}
+
+
+function calculatesFarePrice(start, destination){
+  const distanceTravelled = Math.abs(start-destination)* 264
+  let fare ;{
+
+    if (distanceTravelled < 400) {
+      fare = 0;
+  } 
+  else if (distanceTravelled > 401 && distanceTravelled < 2000 ) {
+      fare = (distanceTravelled -400) * 0.02;
+  }
+  else if (  distanceTravelled > 2000 && distanceTravelled <2500  ){
+      fare = 25 ;
+  }
+  else { 
+      fare = "cannot travel that far";
+  }
+  }
+  return fare;
+
+  
+}
